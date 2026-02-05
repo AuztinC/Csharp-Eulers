3c6f13fb04f22df15e708c50ef4c59afae8b814e
austincripe <aaustinc@live.com>
2026-02-05 11:41:15 -0500

    problem 001 finish

--- Euler.Tests/Problem001Tests.cs
+++ Euler.Tests/Problem001Tests.cs
@@ -2,9 +2,33 @@ using Euler.Console.Problems;
 public class Problem001Tests
 {
     [Fact]
-    public void Solves_Problem_001_Correctly()
+    public void Multiples_Of_3_Or_5_Below_10()
     {
-        var result = Problem001.Solve();
-        Assert.Equal("1", result);
+        var result = Problem001.Find_Multiples(10);
+        Assert.Equal([3, 5, 6, 9], result);
+    }
+
+    [Fact]
+    public void Adds_Multiples_Under_10()
+    {
+        var multiples = Problem001.Find_Multiples(10);
+        var result = Problem001.Add_Multiples(multiples);
+        Assert.Equal(23, result);
+    }
+
+    [Fact]
+    public void Under_1000()
+    {
+        var multiples = Problem001.Find_Multiples(1000);
+        var result = Problem001.Add_Multiples(multiples);
+        Assert.Equal(233168, result);
+    }
+
+    [Fact]
+    public void Under_100()
+    {
+        var multiples = Problem001.Find_Multiples(100);
+        var result = Problem001.Add_Multiples(multiples);
+        Assert.Equal(2318, result);
     }
 }
